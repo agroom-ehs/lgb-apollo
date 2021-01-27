671e9a81c7bde3b92a0095dae7c2ad4253a167be
Jamesy <james.dornan@engagehealth.uk>
2021-01-27 16:42:42 +0000

    added login and refresh token mutations

--- server/index.js
+++ server/index.js
@@ -1,39 +1,36 @@
 import 'dotenv/config'
 import express from 'express'
-import { ApolloServer, gql } from 'apollo-server-express'
+import { ApolloServer } from 'apollo-server-express'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { verify } from 'jsonwebtoken'
 import { typeDefs } from './type_defs/index'
-import { UserResolver } from './resolvers/user_resolver'
+import { resolvers } from './resolvers/index'
+import { createAccessToken, createRefreshToken } from './auth'
 //import { createAccessToken, createRefreshToken } from './auth'
 //import { sendRefreshToken } from './sendRefreshToken'
 (async () => {
-const app = express()
-app.use(
-  cors({
-    origin: 'http://localhost:4000',
-    credentials: true,
+
+  const app = express()
+  app.use(cookieParser())
+  
+  app.use(
+    cors({
+      origin: 'http://localhost:4000',
+      credentials: false,
+    })
+  )
+
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req, res }) => ({ req, res})},
+  )
+
+  apolloServer.applyMiddleware({ app, path:'/', cors: false })
+
+  app.listen(4000, () => {
+    console.log(`🚀 Server ready at http://localhost:4000${apolloServer.graphqlPath}`);
   })
-)
-
-app.use(cookieParser())
-
-const apolloServer = new ApolloServer({
-  typeDefs,
-  UserResolver
-},{
-context: ({ req, res }) => ({
-  req,
-  res,
-}),
-})
-
-
-apolloServer.applyMiddleware({ app, cors: false })
-
-app.listen(4000, () => {
-  console.log('Express server started on port 4000')
-})
 
 })()
